80b9a88e26e5afb5d8c897ba54d01bec46dd67f2
ankur srivastava <ankur.srivastava@techchefz.com>
2024-09-19 15:32:32 +0530

    initial commit

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,12 @@
-import localFont from "next/font/local";
-import "./globals.css";
+// import { AlbertSans } from "next/font/google"
+import "../styles/global.scss";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+
+// export const albertsans = AlbertSans({
+//   subsets: ["latin"],
+//   display: "swap",
+//   variable: "--font-primary",
+// })
 
 export const metadata = {
   title: "Create Next App",
@@ -21,7 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        // className={`${albertsans.variable}`}
       >
         {children}
       </body>
